72eee7c8b8fe69661c0045aa277a3d80ae9c14d6
Jair José Suárez López <jair@elemento43.com>
2019-08-13 21:31:05 -0500

    Log out when the session expires

--- src/app/interceptors/my-http-interceptor.ts
+++ src/app/interceptors/my-http-interceptor.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class MyHttpInterceptor implements HttpInterceptor {
+
+	constructor(private router: Router) { }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // Server ip: 159.89.51.90
         const mUrl = 'http://159.89.51.90:3000';
         req = req.clone({
             url: mUrl + req.url
         });
-        console.log(req);
-        return next.handle(req);
+		return next.handle(req).pipe(tap((res: any) => {
+			if (res.body && res.body.message && res.body.message == 'Fallo la Autenticacion') {
+				localStorage.clear();
+				this.router.navigate(['login'])
+			}
+		}));
     }
 }
